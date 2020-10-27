f6e938ab0ac96ec0bdb97bede41cc177dbd3ced2
ynbylkl <1305984923@qq.com>
2020-10-27 18:32:46 +0800

    新建项目

---
+++ src/pages/custom/components/customViewInfo.vue
@@ -0,0 +1,255 @@
+<template>
+  <div>
+    <a-modal title='基本信息' :visible="customViewInfo" @cancel="closeModal"
+             @ok="submitData" :maskClosable="false" width="60%">
+      <a-form-model ref="formRef" :model="formData" :rules="formRules"
+                    :labelCol="{span:6}" :wrapperCol="{span:12}">
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="姓名" prop="customName">
+              <a-input placeholder="请输入姓名" v-model="formData.customName"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="性别" prop="gender">
+              <a-select v-model="formData.gender" placeholder="请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="出生日期" prop="dateBirth">
+              <a-date-picker v-model="formData.dateBirth"/>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="现居地" prop="livingPlace">
+              <a-input placeholder="" v-model="formData.livingPlace"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="家乡所在地" prop="nativePlace">
+              <a-input placeholder="" v-model="formData.nativePlace"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="身高" prop="height">
+              <a-input placeholder="请输入身高" v-model="formData.height"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="体重" prop="weight">
+              <a-input placeholder="请输入体重" v-model="formData.weight"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="学历" prop="education">
+              <a-select v-model="formData.education" placeholder="请选择">
+                <a-select-option v-for="item in educationList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="职业" prop="profession">
+              <a-select v-model="formData.profession" placeholder=请选择学历请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="月收入" prop="monthIncome">
+              <a-input placeholder="" v-model="formData.monthIncome"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="房产情况" prop="house">
+              <a-input placeholder="" v-model="formData.house"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="车辆情况" prop="isCar">
+              <a-input placeholder="" v-model="formData.isCar"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="有无婚史" prop="marriage">
+              <a-select v-model="formData.gender" placeholder=请选择学历请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="有无小孩" prop="children">
+              <a-select v-model="formData.gender" placeholder=请选择学历请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="是否接受离异" prop="divorce">
+              <a-select v-model="formData.gender" placeholder=请选择学历请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="个人介绍" prop="personal" :labelCol="{span:2}" :wrapperCol="{span: 20}">
+              <a-input placeholder="" v-model="formData.personal"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="家庭介绍" prop="family" :labelCol="{span:2}" :wrapperCol="{span: 20}">
+              <a-input placeholder="" v-model="formData.family"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="择偶标准" prop="standard" :labelCol="{span:2}" :wrapperCol="{span: 20}">
+              <a-input placeholder="" v-model="formData.standard"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="不接受情况" prop="reject" :labelCol="{span:2}" :wrapperCol="{span: 20}">
+              <a-input placeholder="" v-model="formData.reject"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="联系电话" prop="telphone">
+              <a-input placeholder="" v-model="formData.telphone"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="微信号" prop="wechat">
+              <a-input placeholder="" v-model="formData.wechat"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="照片" prop="picture">
+              <a-input placeholder="" v-model="formData.picture"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="介绍次数" prop="number">
+              <a-input placeholder="" v-model="formData.number"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="审核状态" prop="auditStatus">
+              <a-input placeholder="" v-model="formData.auditStatus"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="婚恋状态" prop="status">
+              <a-select v-model="formData.gender" placeholder=请选择学历请选择">
+                <a-select-option v-for="item in genderList" :key="item.id" :value="item.id">{{item.name}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "customViewInfo.vue",
+    data() {
+      return {
+        formData: {},
+        customViewInfo: false,
+        formRules: {},
+        genderList:[{id:'1',name:'男'},{id:'2',name:'女'}],
+        educationList:[{
+          id: '1',
+          name: '博士'
+        }, {
+          id: '2',
+          name: '硕士'
+        }, {
+          id: '3',
+          name: '本科'
+        }, {
+          id: '4',
+          name: '大专'
+        }, {
+          id: '5',
+          name: '中专'
+        },{
+          id: '6',
+          name: '初中及以下'
+        },{
+          id: '7',
+          name: '文盲或半文盲'
+        }],
+      }
+    },
+    mounted() {
+      this.getData();
+    },
+    methods: {
+      submitData() {
+        this.$refs.checkForm.validate(valid => {
+          if (valid) {
+            console.log('check ok, to submit');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      getData() {
+
+      },
+      // 关闭弹窗
+      closeModal() {
+        this.customViewInfo = false;
+        this.$refs.formRef.resetFields();
+      },
+
+      rowClick(record, index) {
+        return {
+          on: {
+            click: () => {
+              console.log(record, index)
+              this.formData = record;
+              this.isDrawerShow = true;
+            }
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/custom/customInfo.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div class="topToolBar">
+      <a-button class="btn_success" @click="showAddNew">新增</a-button>
+      <a-button type="primary" icon="import">导入</a-button>
+    </div>
+    <a-table bordered :row-key="record => record.UserID" :data-source="tableData" :columns="columns"
+             :customRow="rowClick">
+      <img class="tableImg" :onerror="$global.defaultImg" slot="pic" slot-scope="text" :src="text"/>
+    </a-table>
+    <a-pagination
+      v-if="pagination.total > 0"
+      :total="pagination.total"
+      show-size-changer
+      show-quick-jumper
+      v-model="searchForm.pageIndex"
+      :page-size="searchForm.pageSize"
+      :page-size-options="pagination.pageSizes"
+      @showSizeChange="showSizeChange"
+      @change="change"
+      :showTotal="() => `共 ${pagination.total} 条`"
+    />
+
+    <custom-view-info ref="viewChild"></custom-view-info>
+  </div>
+</template>
+
+<script>
+  import customViewInfo from "./components/customViewInfo";
+
+  const columns = [{
+    title: '编号',
+    dataIndex: 'HeadImg',
+  },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+    },
+    {
+      title: '性别',
+      dataIndex: 'age',
+    },
+    {
+      title: '年龄',
+      dataIndex: '',
+    },
+    {
+      title: '身高',
+      dataIndex: '',
+    },
+    {
+      title: '体重',
+      dataIndex: '',
+    },
+    {
+      title: '职业',
+      dataIndex: '',
+    },
+    {
+      title: '月收入',
+      dataIndex: '',
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+    },
+  ];
+
+  export default {
+    components:{customViewInfo},
+    data() {
+      const tableData = [];
+      return {
+        // 表格
+        tableData,
+        columns,
+        loading: false,
+        // 分页对象
+        pagination: {
+          total: 0,
+          pageSizes: ['10', '20', '30', '40', '50'],
+        },
+      }
+    },
+    mounted() {
+      this.getData();
+    },
+    methods: {
+      submitData() {
+        this.$refs.checkForm.validate(valid => {
+          if (valid) {
+            console.log('check ok, to submit');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      getData() {
+
+      },
+      showAddNew() {
+        this.$refs.viewChild.customViewInfo = true;
+      },
+
+      rowClick(record, index) {
+        return {
+          on: {
+            click: () => {
+              console.log(record, index)
+              this.formData = record;
+              this.isDrawerShow = true;
+            }
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .moduleAll {
+  }
+</style>
